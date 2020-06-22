1d499a548473aba4b07c3f53ee417a85ee91ecfe
Saad Aslam <saadusufzai@gmail.com>
2020-06-23 00:23:11 +0500

    css update

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,19 +1,17 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
-// Initial state
+
 const initialState = {
   transactions: []
 }
 
-// Create context
+
 export const GlobalContext = createContext(initialState);
 
-// Provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  // Actions
   function deleteTransaction(id) {
     dispatch({
       type: 'DELETE_TRANSACTION',
